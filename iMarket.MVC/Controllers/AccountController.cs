5580330251869fd74c609c55a880aecd85489b57
Ralph Wesley Baitinga Macedo da Silva <ralphwbms@yahoo.com.br>
2017-11-15 19:02:01 -0200

    Correção de referências às classes no IdentityModel.cs.

--- iMarket.MVC/Controllers/AccountController.cs
+++ iMarket.MVC/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using iMarket.MVC.ViewModels;
+using iMarket.MVC.Models;
 
 namespace iMarket.MVC.Controllers
 {
