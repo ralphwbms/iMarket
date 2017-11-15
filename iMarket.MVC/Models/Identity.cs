f9b35580dd5047f8662c7a57b9575dc84099b4fc
Ralph Weley B. M. Silva <ralphwbms@yahoo.com.br>
2017-11-15 21:39:46 -0200

    EF Context class separado para um arquivo próprio dele.

--- iMarket.MVC/Models/Identity.cs
+++ iMarket.MVC/Models/Identity.cs
@@ -17,17 +17,4 @@ namespace iMarket.MVC.Models
             return userIdentity;
         }
     }
-
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("iMarketDB", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
 }
