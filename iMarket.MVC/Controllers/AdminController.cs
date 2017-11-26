79029bffc4fd2303716ede4c042114fb0003cb6b
Ralph Weley B. M. Silva <ralphwbms@yahoo.com.br>
2017-11-26 16:52:55 -0200

    Novo controlador para a sessão de administração.

---
+++ iMarket.MVC/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using iMarket.Infra.Repositories;
+using iMarket.Models;
+
+namespace iMarket.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        private EFSupermercadoRepository supermercadoRepo;
+
+        public AdminController()
+        {
+            supermercadoRepo = new EFSupermercadoRepository();
+        }
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
