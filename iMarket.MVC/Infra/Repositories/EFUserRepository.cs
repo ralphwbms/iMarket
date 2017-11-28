2766c4a08bd695c29a3d2191f652e3bc566cc4ec
Ralph Weley B. M. Silva <ralphwbms@yahoo.com.br>
2017-11-28 00:16:36 -0200

    Criado repositóirio para a entidade Users.

---
+++ iMarket.MVC/Infra/Repositories/EFUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using iMarket.Infra.Context;
+using iMarket.Models;
+
+namespace iMarket.Infra.Repositories
+{
+    public class EFUserRepository
+    {
+        private IMarketDBContext Db = new IMarketDBContext();
+
+        public IEnumerable<User> Users
+        {
+            get { return Db.Users; }
+        }
+    }
+}
